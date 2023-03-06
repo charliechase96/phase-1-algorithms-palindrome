c442a2a6bc7d10844e948a846bc69d75a9ed5f54
Charlie Gray <charliechasegray@gmail.com>
2023-03-05 19:49:48 -0700

    complete lab

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  return word === word.split("").reverse().join("");
 }
 
 /* 
   Add your pseudocode here
+
+  * split the letters up
+  * reverse letter order
+  * rejoin letters
+  * return compare result with original word
 */
 
 /*
   Add written explanation of your solution here
+
+  * use .split() to split letters of word up
+  * use .reverse() to reverse letters of word order
+  * use .join() to rejoin letters into "new" word
+  * use === to compare result with original word; returns boolean
 */
 
 // You can run `node index.js` to view these console logs
